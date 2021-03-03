48eb008fb69da79ba5c48cf7dc92d730876dd4ac
♪ hiljusti 🎮 <hiljusti@pm.me>
2021-03-02 21:33:31 -0800

    Add clap and a placeholder CLI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use clap::{App, Arg, SubCommand};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("rank")
+        .version("1.0")
+        .about("An organizational tool")
+        .arg(
+            Arg::with_name("config")
+                .short("c")
+                .long("config")
+                .value_name("FILE")
+                .help("Sets a custom config file")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("v")
+                .short("v")
+                .multiple(true)
+                .help("Sets the level of verbosity"),
+        )
+        .arg(
+            Arg::with_name("INPUT")
+                .help("Sets the input file to use")
+                .required(true)
+                .index(1),
+        )
+        .subcommand(
+            SubCommand::with_name("test")
+                .about("controls testing features")
+                .arg(
+                    Arg::with_name("debug")
+                        .short("d")
+                        .help("print debug information verbosely"),
+                ),
+        )
+        .get_matches();
+
+    println!("Matches: {:?}", matches);
 }
