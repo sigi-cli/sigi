5db4e7a4a3ea2fe7145ca3c8434fddc833ce131e
♪ hiljusti 🎮 <hiljusti@pm.me>
2021-03-09 00:20:08 -0800

    Try a stab at simple lifecycle args

--- src/main.rs
+++ src/main.rs
@@ -7,54 +7,48 @@ use std::{env, fs, iter};
 struct Item {
     name: String,
     created: DateTime<Local>,
-    done: Option<DateTime<Local>>
+    succeeded: Option<DateTime<Local>>,
+    failed: Option<DateTime<Local>>
 }
 
 type Items = Vec<Item>;
 
 const RANK_DATA_PATH: [&str; 3] = [".local", "share", "sigi"];
+const CREATE_ALIASES: [&str; 3] = ["do", "start", "new"];
+const COMPLETE_ALIASES: [&str; 3] = ["done", "finish", "fulfill"];
+const DELETE_ALIASES: [&str; 3] = ["drop", "abandon", "retire"];
 
 fn main() {
     let matches = App::new("sigi")
         .version("1.0")
         .about("An organizational tool")
-        .arg(
+        .args(vec![
             Arg::with_name("topic")
                 .short("t")
                 .long("topic")
                 .value_name("TOPIC")
                 .help("Manage items in a specific topic.")
                 .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("v")
-                .short("v")
-                .multiple(true)
-                .help("Sets the level of verbosity"),
-        )
-        .subcommand(
-            SubCommand::with_name("test")
-                .about("controls testing features")
-                .arg(
-                    Arg::with_name("debug")
-                        .short("d")
-                        .help("print debug information verbosely"),
-                ),
-        )
-        .arg(
-            Arg::with_name("INPUT")
-                .help("Sets the input file to use")
-                .required(true)
-                .multiple(true)
-                .index(1),
-        )
+        ])
+        .subcommands(vec![
+            SubCommand::with_name("create")
+                .about("Creates a new item.")
+                .aliases(&CREATE_ALIASES)
+                .arg(Arg::with_name("ITEM").required(true).multiple(true)),
+            SubCommand::with_name("complete")
+                .about("Marks the current item as successfully completed.")
+                .aliases(&COMPLETE_ALIASES),
+            SubCommand::with_name("delete")
+                .about("Marks the current item as unsuccessfully completed.")
+                .aliases(&DELETE_ALIASES)
+        ])
         .get_matches();
 
     println!("Matches: {:?}", matches);
 
     let data_path: String = sigi_data_file("test.json");
 
-    let items: Items = vec![Item { name: String::from("John Cena"), created: Local::now(), done: None }];
+    let items: Items = vec![Item { name: String::from("John Cena"), created: Local::now(), succeeded: None, failed: None }];
 
     if let Ok(Ok(something)) = serde_json::to_string(&items).map(|s| fs::write(data_path.clone(), s)) {
         println!("Wrote something: {:?}", something);
@@ -72,4 +66,3 @@ fn sigi_data_file(filename: impl Into<String>) -> String {
         .collect::<Vec<_>>()
         .join(&std::path::MAIN_SEPARATOR.to_string())
 }
-
