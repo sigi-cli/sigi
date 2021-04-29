38365d0b8e9c4c448fa181f99ac7fe1ebc030707
♪ hiljusti 🎮 <hiljusti@pm.me>
2021-04-28 23:05:12 -0700

    Bump to version 0.2.6

--- src/cli.rs
+++ src/cli.rs
@@ -4,8 +4,8 @@ use clap::{App, Arg, SubCommand};
 use Action::*;
 
 // TODO: Get the version from Cargo.toml? (If possible, at compile time)
-/// The current version (0.2.5) of the CLI.
-pub const SIGI_VERSION: &str = "0.2.5";
+/// The current version (0.2.6) of the CLI.
+pub const SIGI_VERSION: &str = "0.2.6";
 
 const DEFAULT_STACK_NAME: &str = "sigi";
 
