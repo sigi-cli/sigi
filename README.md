a7bcbb8d6744c3563651ccb871582a8879799c7a
J.R. Hill <hiljusti@so.dang.cool>
2022-03-26 08:56:48 -0700

    Start organizing

---
+++ README.md
@@ -0,0 +1,2 @@
+TODO: Fill in some details
+
---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+>&2 echo 'Not yet implemented'
+
+>&2 echo 'General idea is: cp port-src to /usr/port/[etc], cd, and build. Maybe use poudriere? See also https://docs.freebsd.org/en/books/porters-handbook/testing/'
+
+exit 1
+
