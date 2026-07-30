1feb4fe93422f88971654ae1af502d208747f57e
Justin "J.R." Hill <justin@so.dang.cool>
2026-07-30 09:50:33 -0700

    Update expected version in sigi_version test

--- tests/basic_sigi_tests.rs
+++ tests/basic_sigi_tests.rs
@@ -6,6 +6,6 @@ use run_sigi::sigi;
 fn sigi_version() {
     let res = sigi("_integ::version", &["--version"]);
     res.assert_success();
-    res.assert_stdout_line_starts_with("sigi 3.7");
+    res.assert_stdout_line_starts_with("sigi 3.8");
     res.assert_stderr_empty();
 }
